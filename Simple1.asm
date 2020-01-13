1853216be4c5d40f01cdcb983c5409dad7be81e5
jjw3517 <jjw3517@skbl-422-003.ph.ic.ac.uk>
2020-01-13 11:29:04 +0000

    boobs

--- Simple1.asm
+++ Simple1.asm
@@ -7,13 +7,18 @@
 	org 0x100		    ; Main code starts here at address 0x100
 
 start
-	movlw 	0x0
+	movlw	0x0
 	movwf	TRISC, ACCESS	    ; Port C all outputs
+	movlw	0xFF
+	movwf	TRISD, ACCESS	    ; Port D Setup
+	bsf	PADCFG1,RDPU, ACCESS
+	movlw 	0x0
 	bra 	test
 loop	movff 	0x06, PORTC
 	incf 	0x06, W, ACCESS
 test	movwf	0x06, ACCESS	    ; Test for end of loop condition
-	movlw 	0x0A
+	
+	movf	PORTD, W
 	cpfsgt 	0x06, ACCESS
 	bra 	loop		    ; Not yet finished goto start of loop again
 	goto 	0x0		    ; Re-run program from start
