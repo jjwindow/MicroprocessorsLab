4e86fc8c23183b88d833395409b712bc0be5e66e
jjw3517 <jjw3517@skbl-422-003.ph.ic.ac.uk>
2020-01-13 16:24:12 +0000

    boobs

--- Simple1.asm
+++ Simple1.asm
@@ -9,18 +9,37 @@
 start
 	movlw	0x0
 	movwf	TRISC, ACCESS	    ; Port C all outputs
-	movlw	0xFF
+	movlw	0xFF		    ; Initialise PORTD switches as inputs
 	movwf	TRISD, ACCESS	    ; Port D Setup
 	bsf	PADCFG1,RDPU, ACCESS
-	movlw 	0x0
+	movlw 	0x0		    ; clear W
 	bra 	test
-loop	movff 	0x06, PORTC
+	
+loop	movlw	high(0xFFFF)		    ; delay by 16 instructions
+	movwf	0x20
+	movlw	low(0xFFFF)
+	movwf	0x21
+	;call	delay		    ; Run delay
+	call	longdelay
+	movff 	0x06, PORTC
 	incf 	0x06, W, ACCESS
+	
 test	movwf	0x06, ACCESS	    ; Test for end of loop condition
 	
 	movf	PORTD, W
-	cpfsgt 	0x06, ACCESS
+	cpfsgt 	0x06, ACCESS	    ; Compare count value to input
 	bra 	loop		    ; Not yet finished goto start of loop again
 	goto 	0x0		    ; Re-run program from start
+
+delay	decfsz	0x20		    ; delay decrement
+	bra delay
+	return
+longdelay
+	movlw	0x0
+dloop	decf	0x21,f
+	subwfb	0x20,f
+	bc dloop
+	return
+
 	
 	end
