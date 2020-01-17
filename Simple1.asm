27eef2ba0444c84a6626453763a4f6fbc3968df7
jjwindow <53532611+jjwindow@users.noreply.github.com>
2020-01-17 14:19:32 +0000

    Add files via upload

--- Simple1.asm
+++ Simple1.asm
@@ -7,15 +7,44 @@
 	org 0x100		    ; Main code starts here at address 0x100
 
 start
-	movlw 	0x0
-	movwf	TRISB, ACCESS	    ; Port C all outputs
+	movlw	0x0
+	movwf	TRISC, ACCESS	    ; Port C all outputs
+	movlw	0xFF		    ; Initialise PORTD switches as inputs
+	movwf	TRISD, ACCESS	    ; Port D Setup
+	bsf	PADCFG1,RDPU, ACCESS
+	movlw	0x10
+	movwf	0x30		    ; set FR0x30 to .16
+	movlw 	0x0		    ; clear W
 	bra 	test
-loop	movff 	0x06, PORTB
+	
+loop	movlw	high(0xFFFF)		    ; delay by 16 instructions
+	movwf	0x20
+	movlw	low(0xFFFF)
+	movwf	0x21
+	;call	delay		    ; Run delay
+	call	longdelay
+	movff 	0x06, PORTC
 	incf 	0x06, W, ACCESS
+	
 test	movwf	0x06, ACCESS	    ; Test for end of loop condition
-	movlw 	0x63
-	cpfsgt 	0x06, ACCESS
+	
+	movf	PORTD, W
+	cpfsgt 	0x06, ACCESS	    ; Compare count value to input
 	bra 	loop		    ; Not yet finished goto start of loop again
 	goto 	0x0		    ; Re-run program from start
+
+delay	decfsz	0x30		    ; delay decrement
+	bra delay
+	return
+longdelay
+	movlw	0x0
+dloop	;movlw	0x10
+	;movwf	0x30
+	;call	delay
+	decf	0x21,f
+	subwfb	0x20,f
+	bc dloop
+	return
+
 	
 	end
