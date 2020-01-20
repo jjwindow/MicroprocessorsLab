c90ea20bcb84ba65078245a274f4d501238bbb52
jjw3517 <jjw3517@skbl-422-003.ph.ic.ac.uk>
2020-01-20 16:38:49 +0000

    same but it works this time

--- Simple1.asm
+++ Simple1.asm
@@ -12,6 +12,7 @@ start
 	bsf	PADCFG1, REPU, BANKED ; PortE pull-ups on
 	movlb	0x00 ; set BSR back to Bank 0
 	clrf	TRISD ; clear TRISD
+	setf	PORTD	; initialise control bus
 	movlw	0x2
 	movwf	0x30 ; set FR0x30 to 2
 	movlw 	0x0
@@ -22,10 +23,10 @@ write_RAM1
 	movlw	0x5
 	movwf	LATE
 	movlw	0x1
-	movwf	PORTD
+	movwf	PORTE
 	call	delay
 	movlw	0x3
-	movwf	PORTD
+	movwf	PORTE
 	setf	TRISE
 	goto	read_RAM1
 
@@ -33,11 +34,11 @@ write_RAM2
 	clrf	TRISE
 	movlw	0x6
 	movwf	LATE
-	bcf	PORTD, 0x2
+	bcf	PORTD, 3
 	movlw	0x6
 	movwf	0x30
 	call	delay
-	bsf	PORTD, 0x2
+	bsf	PORTD, 3
 	setf	TRISE
 	goto	read_RAM2
 	
@@ -48,10 +49,10 @@ read_RAM1
 	movwf	PORTD
 	
 read_RAM2	
-	bcf	PORTD, 0x2
+	bcf	PORTD, 2
 	movlw	0x00
-	call	delaydelay
-	bsf	PORTD, 0x3
+	;call	delaydelay
+	bsf	PORTD, 2
 
 delay	movwf	0x30
 dloop	decfsz	0x30		    ; delay decrement
