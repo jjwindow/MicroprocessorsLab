c1fd40d283fc3e192607c15c5a3898279ff643ec
jjw3517 <jjw3517@skbl-422-003.ph.ic.ac.uk>
2020-01-27 14:20:49 +0000

    Read row/columns written

--- Keypad.asm
+++ Keypad.asm
@@ -1,13 +1,41 @@
 #include p18f87k22.inc
+    
+    global  Keypad_Setup, Read_Row, Read_Column
 
-    global  ;
-
+    org 0x100
 acs0    udata_acs	    ; named variables in access ram
-; res 1	    ; reserve 1 byte for variable UART_counter
+delay_count res 1   ; reserve one byte for counter in the delay routine
 
 Keypad   code
     
 Keypad_Setup
-    bsf PADCFG1,REPU,banked
-    clrf LATE ;WRITE 0S TO LATE (do this once)
-    Set TRISE to 0x0F ;configure pins 0-3 as INPUTS & 4-7 as OUTPUTS
+    banksel PADCFG1 ;PADCFG1 is not in Access Bank!!
+    bsf	    PADCFG1,REPU, BANKED
+    clrf    LATE ;WRITE 0S TO LATE (do this once)
+    
+    movlw 0x00
+    movwf delay_count
+    return
+    
+    
+Read_Row
+    ; configure pins 0-3 as inputs & 4-7 as Outputs
+    movlw 0x0F
+    movwf TRISE
+    call delay
+    return
+    
+Read_Column
+    ; configure pins 0-3 as outputs & 4-7 as Inputs
+    movlw 0xF0
+    movwf TRISE
+    call delay
+    return
+    
+	; a delay subroutine if you need one, times around loop in delay_count
+delay	decfsz	delay_count	; decrement until zero
+	bra delay
+	return
+
+
+end
