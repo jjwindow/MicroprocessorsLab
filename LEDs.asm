0f6292cf88c59e14a25a2c0f415cab5f9c192996
jjw3517 <jjw3517@skbl-422-003.ph.ic.ac.uk>
2020-02-07 10:04:19 +0000

    send 1, 0 routines written, not tested.

--- LEDs.asm
+++ LEDs.asm
@@ -6,6 +6,7 @@
 acs0	udata_acs
 
 BRIGHTNESS	res 1
+bitcount	res 1
 	
 LEDs	code
     
@@ -18,16 +19,55 @@ LED_Setup
 
 Output_GRB
 	bcf	INTCON,GIE ;disable interrupts
-	
-	goto Output_GRB
+	movlw	.150
+	movwf	bitcount
+loop	; bit testing, 1 or 0
+	goto loop
 	
 
 	
 Send_1
 	; 0.8us HI, 0.45us LO 
-	
+	; 20 instructions TOTAL (after bsf)
+	bsf	PORTE, 0
+	call	delay_.8    ; 12 instruction delay		    
+	bcf	PORTE, 0    ; 12 instructions -> 13 instructions, lo pulse sent
+	call	delay_.4    ; 13 - 19
+	NOP
+	bsf PORTE, 0	    ; End hi	
 	
 Send_0
 	; 0.4us HI, 0.85us LO 
+	; 20 instructions TOTAL (after bsf)
+	bsf	PORTE, 0
+	call	delay_.4		    
+	bcf	PORTE, 0    ; 6 instruction delay -> 7 instruction delay, lo pulse sent
+	call	delay_.85
+	bsf PORTE, 0	    ; End hi
 	
+	
+	
+delay_.4
+	NOP		; 6 instruction delay
+	NOP		; req'd delay/time per instruction = 400ns/62.5ns 
+	NOP		; = 6.4 (+/- 2) instructions.
+	NOP
+	return
+	
+delay_.45
+	
+delay_.8
+	call	delay_.4    ; instructions 2-7
+	NOP
+	NOP
+	NOP
+	NOP
+	return
+	
+	
+	
+delay_.85
+	call	delay_.4    ; instructions 9 - 14
+	call	delay_.4    ; instructions 14 - 20 
+	return
 end
