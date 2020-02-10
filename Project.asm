b26f54cae5d99f97158ddd79723c4ad08d8087e7
jjw3517 <jjw3517@skbl-422-003.ph.ic.ac.uk>
2020-02-10 16:40:42 +0000

    not working. Move bit testing inside bit sending routines

--- Project.asm
+++ Project.asm
@@ -1,6 +1,6 @@
 #include p18f87k22.inc
   
-extern LED_Setup,Output_GRB
+    extern LED_Setup,Output_GRB
 	
 rst	code	0x0000	; reset vector
 	goto	start
@@ -8,7 +8,7 @@ rst	code	0x0000	; reset vector
 main	code
 start	call	LED_Setup		; Sit in infinite loop
 	call	Output_GRB
-	goto	rst
+	goto	$
 	end
 
 
