b9e8b6d9ef2a158460dbafe797e168448edbcb1c
jjw3517 <jjw3517@skbl-422-003.ph.ic.ac.uk>
2020-02-10 15:54:43 +0000

    TESTING DATA SENDING

---
+++ Project.asm
@@ -0,0 +1,14 @@
+#include p18f87k22.inc
+  
+extern LED_Setup,Output_GRB
+	
+rst	code	0x0000	; reset vector
+	goto	start
+	
+main	code
+start	call	LED_Setup		; Sit in infinite loop
+	call	Output_GRB
+	goto	$
+	end
+
+
